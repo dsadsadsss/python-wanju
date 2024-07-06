4fb2903d67a02e7262888cdbf1242b0e89ecc86e
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2024-07-06 13:41:38 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,33 +1,31 @@
 #!/bin/bash
 
-# 节点可在list.log中查看或手搓节点,默认端口vless:8002，vmess:8001
-#=======================================
-# 设置固定隧道参数(如果不设置，默认启用临时隧道，端口自动，无需手动设置)
-# json或token,支持直接复制全部，如cloudflared.exe service install eyxxx，F大佬json获取地址https://fscarmen.cloudflare.now.cc
-export TOK='' 
-export ARGO_DOMAIN=''
-#=======================================
-#设置哪吒参数(NEZHA_TLS='1'开启tls,设置其他关闭tls)
-export NEZHA_SERVER=''
-export NEZHA_KEY=''
-export NEZHA_PORT='443'
-export NEZHA_TLS='1'
-#=======================================
-# 下面设置UUID和路径，CF_IP是优选IP，SUB_NAME为节点名称
-export UUID='fd80f56e-93f3-4c85-b2a8-c77216c509a7'
-export VPATH='vls' #vless路径
-export MPATH='vms' #vmess路径
-export CF_IP='ip.sb'
-export SUB_NAME='bbn'
-#=======================================
-# 启动玩具原程序，把原游戏文件改名senver.jar,默认不开启，如果开启请去掉前面的#号.
-# 注意原游戏启动后，不显示节点日志
-# export JAR_SH='java -Xms128M -XX:MaxRAMPercentage=95.0 -jar senver.jar' #复制原启动命令填入并修改启动文件名为senver.jar
-#=======================================
-# 设置是否显示日志，默认显示，填no不显示，yes显示
-# export RIZHI='no'
-#=======================================
+# 设置ARGO参数 (不设置则临时隧道，固定隧道设置端口8002,协议vless,ws,tls,路径vls)
+# json获取地址https://fscarmen.cloudflare.now.cc
+export TOK=${TOK:-''} # json或token,支持直接复制cloudflared.exe service开头全部
+export ARGO_DOMAIN=${ARGO_DOMAIN:-''} # 隧道域名
 
+
+# 设置哪吒参数(NEZHA_TLS='1'开启tls,设置0关闭tls)
+export NEZHA_SERVER=${NEZHA_SERVER:-''}
+export NEZHA_KEY=${NEZHA_KEY:-''}
+export NEZHA_PORT=${NEZHA_PORT:-'443'}
+export NEZHA_TLS=${NEZHA_TLS:-'1'}
+
+
+# 设置UUID和路径等
+export UUID=${UUID:-'fd80f56e-93f3-4c85-b2a8-c77216c509a7'}
+export VPATH=${VPATH:-'vls'} 
+export MPATH=${MPATH:-'vms'}
+export CF_IP=${CF_IP:-'ip.sb'} # 优选ip
+export SUB_NAME=${SUB_NAME:-'argo'} # 节点名称
+
+# 设置启动游戏原程序，先把原启动文件server.jar改名成senver.jar，其他游戏也可类似修改启动文件
+# 复制原来的启动命令并改启动文件名为senver.jar填入下面即可
+# export JAR_SH='java -jar senver.jar' # 启用需去掉前面的#号，启用后节点信息不再显示，只显示游戏信息
+
+
+# 启动脚本
 echo "aWYgY29tbWFuZCAtdiBjdXJsICY+L2Rldi9udWxsOyB0aGVuCiAgICAgICAgRE9XTkxPQURfQ01E
 PSJjdXJsIC1zTCIKICAgICMgQ2hlY2sgaWYgd2dldCBpcyBhdmFpbGFibGUKICBlbGlmIGNvbW1h
 bmQgLXYgd2dldCAmPi9kZXYvbnVsbDsgdGhlbgogICAgICAgIERPV05MT0FEX0NNRD0id2dldCAt
